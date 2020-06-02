159ad5d094fa138de90f22d60e1610d3f4c98ad9
Demchenko87 <conder@ukr.net>
2020-06-02 15:33:58 +0300

    fix products bug add

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  get 'store/index'
+  resources :products
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'store#index', as: 'store'
+end
